bfa50151cb1c327e7061867363a4dc732347a695
andi <andi@andilabs.com>
2019-10-15 21:21:44 +0200

    osx on travis sucks; return sad echo message when no osascript available

--- contrib/applescript.py
+++ contrib/applescript.py
@@ -10,4 +10,4 @@ except sh.CommandNotFound:
     sad_message = 'Sorry you are probably not running OS X and ' \
                   'Applescript is not available on this platform'
     logger.warning(sad_message)
-    osascript = None
+    osascript = sh.Command("echo")
